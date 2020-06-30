c49992f4d12fae22514411808857986a3ab3d532
masatokg <masatokg@asojuku.ac.jp>
2020-06-30 05:39:56 +0000

    first git

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <helloWorld v-bind:title="message"
+      v-on:result-event="appAction"/>
+    <hr>
+    <P>{{result}}</P>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data:function(){
+    return{
+      message:'HELLO',
+      result:'no Event',
+    };
+  },
+  methods:{
+    appAction:function(message){
+      this.result = '(*** you send:"' + message +' ****)'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
